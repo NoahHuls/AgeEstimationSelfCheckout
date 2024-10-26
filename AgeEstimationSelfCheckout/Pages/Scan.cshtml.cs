b6a1384c14e9dd6e93471179035d961c829fe658
Noah Huls <hulsnoah@gmail.com>
2024-10-26 22:00:02 +0200

    added custom design stuff

--- AgeEstimationSelfCheckout/Pages/Scan.cshtml.cs
+++ AgeEstimationSelfCheckout/Pages/Scan.cshtml.cs
@@ -1,12 +1,15 @@
 using AgeEstimationSelfCheckout.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
 
 namespace AgeEstimationSelfCheckout.Pages
 {
     public class ScanModel : PageModel
     {
-        public static List<Product> Products { get; set; } = [];
+        public static List<Product> Products { get; set; } = new List<Product>();
+        public static bool AutomaticAgeVerification { get; set; } = false;
+        public static bool AgeVerificationIsAltered { get; set; } = false;
 
         public void OnGet(string action)
         {
@@ -24,17 +27,25 @@ namespace AgeEstimationSelfCheckout.Pages
             }
         }
 
+        public void OnPostToggle(bool isAutomaticAgeVerificationEnabled)
+        {
+            AutomaticAgeVerification = !AutomaticAgeVerification;
+            AgeVerificationIsAltered = true;
+        }
+
         public void AddBread()
         {
             Products.Add(new Product { Name = "Brood", IsAgeRestricted = false, Price = 1.49M });
         }
+
         public void AddBeer()
         {
             Products.Add(new Product { Name = "Bier", IsAgeRestricted = true, Price = 3.49M });
         }
+
         public void AddCheese()
         {
-            Products.Add(new Product { Name = "Kaas", IsAgeRestricted = false, Price = 8.98M });
+            Products.Add(new Product { Name = "Kaas", IsAgeRestricted = false, Price = 2.99M });
         }
     }
 }
